7e3d6439103a70144792656cd9afd9ec5bf75ff9
Gabriel Martinez <gabrielmartinez@lexicacorp.com>
2019-08-01 21:36:09 -0500

    fix remove item

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,50 +1,69 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { connect } from "react-redux";
-import CounterControl from '../../components/CounterControl/CounterControl';
-import CounterOutput from '../../components/CounterOutput/CounterOutput';
+import CounterControl from "../../components/CounterControl/CounterControl";
+import CounterOutput from "../../components/CounterOutput/CounterOutput";
 import {
-    incCounter,
-    decCounter,
-    addCounter,
-    subCounter,
-    storeResult,
-    deleteResult
+  incCounter,
+  decCounter,
+  addCounter,
+  subCounter,
+  storeResult,
+  deleteResult
 } from "../../store/actionCreators";
 class Counter extends Component {
-    state = {
-        counter: 0
-    }
+  state = {
+    counter: 0
+  };
 
-    render() {
-        return (
-            <div>
-                <CounterOutput value={this.props.ctr} />
-                <CounterControl label="Increment" clicked={this.props.onIncrementerCounter} />
-                <CounterControl label="Decrement" clicked={this.props.onDecrementerCounter} />
-                <CounterControl label="Add 5" clicked={() => this.props.onAddCounter(5)} />
-                <CounterControl label="Subtract 5" clicked={() => this.props.onSubCounter(5)} />
-                <hr />
-                <button onClick={this.props.onStoreResult}>Store Result</button>
-                <ul>
-                    {this.props.rslts.map((rst) => <li onClick={() => this.props.onDeleteResult(rst..id)} key={rst.id}>{rst.value}</li>)}
-                </ul>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <CounterOutput value={this.props.ctr} />
+        <CounterControl
+          label="Increment"
+          clicked={this.props.onIncrementerCounter}
+        />
+        <CounterControl
+          label="Decrement"
+          clicked={this.props.onDecrementerCounter}
+        />
+        <CounterControl
+          label="Add 5"
+          clicked={() => this.props.onAddCounter(5)}
+        />
+        <CounterControl
+          label="Subtract 5"
+          clicked={() => this.props.onSubCounter(5)}
+        />
+        <hr />
+        <button onClick={this.props.onStoreResult}>Store Result</button>
+        <ul>
+          {this.props.rslts.map((rst, id) => (
+            <li onClick={() => this.props.onDeleteResult(id)} key={id}>
+              {rst}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => ({
-    ctr: state.counter,
-    rslts: state.results
-})
+  ctr: state.counter,
+  rslts: state.results
+});
 
 const mapDispatchToProps = dispatch => ({
-    onIncrementerCounter: () => dispatch(incCounter()),
-    onDecrementerCounter: () => dispatch(decCounter()),
-    onAddCounter: (value) => dispatch(addCounter(value)),
-    onSubCounter: (value) => dispatch(subCounter(value)),
-    onStoreResult: () => dispatch(storeResult()),
-    onDeleteResult: (index) => dispatch(deleteResult(index))
-})
+  onIncrementerCounter: () => dispatch(incCounter()),
+  onDecrementerCounter: () => dispatch(decCounter()),
+  onAddCounter: value => dispatch(addCounter(value)),
+  onSubCounter: value => dispatch(subCounter(value)),
+  onStoreResult: () => dispatch(storeResult()),
+  onDeleteResult: index => dispatch(deleteResult(index))
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Counter);
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,52 +1,55 @@
 import {
-    INC_COUNTER,
-    ADD_COUNTER,
-    DEC_COUNTER,
-    SUB_COUNTER,
-    STORE_RESULT,
-    DELETE_RESULT
+  INC_COUNTER,
+  ADD_COUNTER,
+  DEC_COUNTER,
+  SUB_COUNTER,
+  STORE_RESULT,
+  DELETE_RESULT
 } from "./actionTypes";
 const initialState = {
-    counter: 0,
-    results: []
-}
-
+  counter: 0,
+  results: []
+};
 
 const reducer = (state = initialState, action) => {
-    switch (action.type) {
-        case INC_COUNTER:
-            return {
-                ...state,
-                counter: state.counter + 1
-            };
-        case ADD_COUNTER:
-            return {
-                ...state,
-                counter: state.counter + action.value
-            };
-        case DEC_COUNTER:
-            return {
-                ...state,
-                counter: state.counter - 1
-            };
-        case SUB_COUNTER:
-            return {
-                ...state,
-                counter: state.counter - action.value
-            }
-        case STORE_RESULT:
-            return {
-                ...state,
-                results: state.results.concat({ id: new Date(), value: state.counter })
-            }
-        case DELETE_RESULT:
-            return {
-                ...state,
-                results: state.results.slice(action.value, 1)
-            }
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case INC_COUNTER:
+      return {
+        ...state,
+        counter: state.counter + 1
+      };
+    case ADD_COUNTER:
+      return {
+        ...state,
+        counter: state.counter + action.value
+      };
+    case DEC_COUNTER:
+      return {
+        ...state,
+        counter: state.counter - 1
+      };
+    case SUB_COUNTER:
+      return {
+        ...state,
+        counter: state.counter - action.value
+      };
+    case STORE_RESULT:
+      return {
+        ...state,
+        results: state.results.concat(state.counter)
+      };
+    case DELETE_RESULT:
+      let newArray = [
+        ...state.results.slice(0, action.index),
+        ...state.results.slice(action.index + 1)
+      ];
+      return {
+        ...state,
+        results: newArray
+      };
+    default:
+      return state;
+  }
+};
 
 export default reducer;
